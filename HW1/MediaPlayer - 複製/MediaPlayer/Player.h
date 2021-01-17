a29e72cf95660c299bc5442a68af2eccb4dd5b2d
elkslkgy <b05902118@ntu.edu.tw>
2021-01-17 15:07:44 +0800

    Delete HW1/MediaPlayer - 複製 directory

--- HW1/MediaPlayer - 複製/MediaPlayer/Player.h
+++
@@ -1,8 +0,0 @@
-#include <dshow.h>
-
-#define WM_GRAPHNOTIFY WM_APP + 1   // Private message.
-
-void BeginPlay(HWND hwnd);
-//DWORD Play(LPVOID lpThreadParameter);
-void Stop();
-bool GetCompletionEvent();
